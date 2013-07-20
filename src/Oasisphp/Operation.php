2dcbe04b813ee38ecec3b0fbd617d567510da6c5
Chris Cornutt <ccornutt@ccornutt.local>
2013-07-20 13:54:51 -0700

    adding docblocks to Operation class

--- src/Oasisphp/Operation.php
+++ src/Oasisphp/Operation.php
@@ -2,36 +2,84 @@
 
 namespace Oasisphp;
 
+/**
+ * Matching operation definition
+ *
+ * @package Oasisphp
+ */
 abstract class Operation
 {
+    /**
+     * Property #1 to evaluate
+     * @var mixed
+     */
     protected $property1 = null;
 
+    /**
+     * Perperty #2 to evaluate
+     * @var mixed
+     */
     protected $property2 = null;
 
+    /**
+     * Construct the operation with the two values
+     *
+     * @param mixed $property1 Property value #1
+     * @param mixed $property2 Property value #2
+     */
     public function __construct($property1, $property2)
     {
         $this->setProperty1($property1);
         $this->setProperty2($property2);
     }
 
+    /**
+     * Set the first property to evaluate
+     *
+     * @param mixed $value Property value
+     */
     public function setProperty1($value)
     {
         $this->property1 = $value;
+        return $this;
     }
+
+    /**
+     * Get the first property value
+     *
+     * @return mixed Property #1 value
+     */
     public function getProperty1()
     {
         return $this->property1;
     }
 
+    /**
+     * Set the second property value
+     *
+     * @param mixed $value Property #2 value
+     * @return \Oasisphp\Operation instance
+     */
     public function setProperty2($value)
     {
         $this->property2 = $value;
         return $this;
     }
+
+    /**
+     * Get the second property value
+     *
+     * @return mixed Property #2 value
+     */
     public function getProperty2()
     {
         return $this->property2;
     }
 
+    /**
+     * Evaluate the properties against acceptance criteria
+     *
+     * @return boolean Pass/fail of evaluation status
+     */
     abstract public function evaluate();
 }
