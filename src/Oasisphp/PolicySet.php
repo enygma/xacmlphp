9702fdf8a0d020f9659a83d940773246f0b78795
Chris Cornutt <ccornutt@ccornutt.local>
2013-07-21 05:33:58 -0700

    adding docblock PolicySet

--- src/Oasisphp/PolicySet.php
+++ src/Oasisphp/PolicySet.php
@@ -2,29 +2,64 @@
 
 namespace Oasisphp;
 
+/**
+ * Set of Policies, complete with it's own combining algorithm
+ *
+ * @package Oasisphp
+ */
 class PolicySet
 {
+    /**
+     * Set of Policies
+     * @var array
+     */
     private $policies = array();
 
+    /**
+     * Combining algorithm
+     * @var \Oasisphp\Algorithm
+     */
     private $algorithm = null;
 
+    /**
+     * Add a new policy to the set
+     *
+     * @param \Oasisphp\Policy $policy Policy object
+     * @return \Oasisphp\PolicySet
+     */
     public function addPolicy(\Oasisphp\Policy $policy)
     {
         $this->policies[] = $policy;
         return $this;
     }
 
+    /**
+     * Get all current policies
+     *
+     * @return array Set of policies
+     */
     public function getPolicies()
     {
         return $this->policies;
     }
 
+    /**
+     * Set the PolicySet combining algorithm
+     *
+     * @param \Oasisphp\Algorithm $algorithm Combining algorithm
+     * @return \Oasisphp\PolicySet
+     */
     public function setAlgorithm(\Oasisphp\Algorithm $algorithm)
     {
         $this->algorithm = $algorithm;
         return $this;
     }
 
+    /**
+     * Get the current combining Algorithm
+     *
+     * @return \Oasisphp\Algorithm instance
+     */
     public function getAlgorithm()
     {
         return $this->algorithm;
