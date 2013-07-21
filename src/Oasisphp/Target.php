2129e93fb622057eeca672d422e369128d4cf218
Chris Cornutt <ccornutt@ccornutt.local>
2013-07-21 06:03:03 -0700

    adding docblock to the Target class

--- src/Oasisphp/Target.php
+++ src/Oasisphp/Target.php
@@ -2,21 +2,49 @@
 
 namespace Oasisphp;
 
+/**
+ * Rule target containing Match set
+ *
+ * @package Oasisphp
+ */
 class Target
 {
+    /**
+     * Set of Matches for a Target
+     * @var array
+     */
     private $matches = array();
 
+    /**
+     * Add a new Match to the Target set
+     *
+     * @param \Oasisphp\Match $match Match instance
+     * @return \Oasisphp\Target
+     */
     public function addMatch(\Oasisphp\Match $match)
     {
         $this->matches[] = $match;
         return $this;
     }
+
+    /**
+     * Add multiple new Matches to the set
+     *
+     * @param array $matches Multiple matches
+     */
     public function addMatches(array $matches)
     {
         foreach ($matches as $match) {
             $this->addMatch($match);
         }
+        return $this;
     }
+
+    /**
+     * Get the current set of Matches
+     *
+     * @return array Matches set
+     */
     public function getMatches()
     {
         return $this->matches;
