d6d6308daa4980c4d156cd40595e492fa7f29291
Chris Cornutt <ccornutt@ccornutt.local>
2013-07-21 06:00:24 -0700

    adding docblock to Subject class

--- src/Oasisphp/Subject.php
+++ src/Oasisphp/Subject.php
@@ -2,14 +2,36 @@
 
 namespace Oasisphp;
 
+/**
+ * Subject of current evaluation
+ *
+ * @package Oasisphp
+ */
 class Subject
 {
+    /**
+     * Attribute set for subject
+     * @var array
+     */
     private $attributes = array();
 
+    /**
+     * Add a new Attribute
+     *
+     * @param \Oasisphp\Attribute $attribute instance
+     * @return \Oasisphp\Subject instance
+     */
     public function addAttribute(\Oasisphp\Attribute $attribute)
     {
         $this->attributes[] = $attribute;
+        return $this;
     }
+
+    /**
+     * Get the current set of Attributes
+     *
+     * @return array Attribute set
+     */
     public function getAttributes()
     {
         return $this->attributes;
