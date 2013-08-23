bfe25002cdad64fe6b7d8b71805096fb0f0fa5a6
Chris Cornutt <ccornutt@ccornutt.local>
2013-08-23 15:36:32 -0700

    adding type setting to the Action

--- src/Oasisphp/Action.php
+++ src/Oasisphp/Action.php
@@ -4,5 +4,41 @@ namespace Oasisphp;
 
 class Action
 {
+    /**
+     * Action type
+     * @var string
+     */
+    private $type = null;
 
+    /**
+     * Init the object and set the type if given
+     *
+     * @param string $type Action type
+     */
+    public function __construct($type = null)
+    {
+        if ($type !== null) {
+            $this->setType($type);
+        }
+    }
+
+    /**
+     * Set the action type
+     *
+     * @param string $type Action type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get the current action type
+     *
+     * @return string Action type
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
 }
